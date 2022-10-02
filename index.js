439bd0865803356b9eb25d0c11db4a0fc9771b7b
Vishal Jaiswal <gauravkmjaiswal.com>
2022-10-02 15:26:07 +0530

    first commit 2

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-
 //components
 import Connection from './database/db.js';
 import Router from './routes/route.js';
-
+const path =require('path')
 
 dotenv.config();
 
@@ -18,10 +17,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', Router);
 
 
-const PORT = 8000;
+// if(process.env.NODE_ENV=='production')
+// {
+
+//     app.get('/',(req,res)=>{
+//         app.use(express.static(path.resolve(__dirname,'client','build','index.html')))
+//         app.sendfile(path.resolve(__dirname,'client','build','index.html'))
+//     })
+// }
+
+const PORT = process.env.Port || 8000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
-Connection(username, password);
-
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+
+const URL = process.env.MONGODB_URI || `mongodb+srv://${username}:${password}@allaboutpendemic.asqmc.mongodb.net/AllAboutPendemic?retryWrites=true&w=majority`;
+Connection(URL);
